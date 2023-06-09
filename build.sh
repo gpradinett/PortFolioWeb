3fa923b5ab8691dde138c901111be1786c139a5b
gpradinett <gpradinett@gmail.com>
2023-06-09 00:29:26 -0500

    add new config

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations Baker      
+python manage.py makemigrations  
 python manage.py migrate
 
 python manage.py createsuperuser --no-input --username=admin2 --email=gpra@gmail.com
