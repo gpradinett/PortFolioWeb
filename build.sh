0e701402b8ebe9f540850a3b56f4e5ecf3891998
gpradinett <gpradinett@gmail.com>
2023-06-09 00:27:55 -0500

    add new config

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations Baker      
 python manage.py migrate
 
-python manage.py createsuperuser --no-input --username=admin1 --email=gpra@gmail.com
+python manage.py createsuperuser --no-input --username=admin2 --email=gpra@gmail.com
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='admin1'); user.set_password('MiPa.contra1!'); user.save()" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='admin2'); user.set_password('MiPa.contra1!'); user.save()" | python manage.py shell
