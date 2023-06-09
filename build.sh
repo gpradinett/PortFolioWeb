d8e6c4c16fa640307e66cdda287ef725cc46428b
gpradinett <gpradinett@gmail.com>
2023-06-09 00:15:00 -0500

    add new config

--- build.sh
+++ build.sh
@@ -9,9 +9,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --no-input --username=admin --email=gpra@gmail.com
 
-expect << EOF
-spawn python manage.py changepassword admin
-expect "Password:"
-send "MiPa.contra1!\r"
-expect eof
-EOF
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='admin'); user.set_password('MiPa.contra1!'); user.save()" | python manage.py shell
