98e79ba7992795bc676de574cb8edfddb5487685
gpradinett <gpradinett@gmail.com>
2023-07-02 20:53:47 -0500

    fix module pytube

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations  
 python manage.py migrate
 
-python manage.py createsuperuser --no-input --username=admin6 --email=gpra@gmail.com
+python manage.py createsuperuser --no-input --username=admin7 --email=gpra@gmail.com
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='admin6'); user.set_password('MiPa.contra1!'); user.save()" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='admin7'); user.set_password('MiPa.contra1!'); user.save()" | python manage.py shell
